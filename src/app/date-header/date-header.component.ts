9ccdc964be7a2c4f5b0e142063450242593d35a4
Cyril Steimer <cyril.steimer@gmail.com>
2018-04-18 20:13:27 +0200

    Convert the date header component to Bootstrap
    Still needs some work to look good, but is functionally ok.

--- src/app/date-header/date-header.component.ts
+++ src/app/date-header/date-header.component.ts
@@ -5,129 +5,129 @@ import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 import * as $ from 'jquery'
 
 @Component({
-  selector: 'app-date-header',
-  templateUrl: './date-header.component.html',
-  styleUrls: ['./date-header.component.css']
+	selector: 'app-date-header',
+	templateUrl: './date-header.component.html',
+	styleUrls: ['./date-header.component.css']
 })
 export class DateHeaderComponent implements OnInit {
 
-  @Input() date: Date
-  @Input() switcher: BudgetPeriodSwitcher
-  @Input() urlPrefix: string
+	@Input() date: Date
+	@Input() switcher: BudgetPeriodSwitcher
+	@Input() urlPrefix: string
 
-  nextDateLink: DateLink
-  dateLink: DateLink
-  prevDateLink: DateLink
-  
-  parent: DateLink
-  children: DateLink[]
+	nextDateLink: DateLink
+	dateLink: DateLink
+	prevDateLink: DateLink
+	
+	parent: DateLink
+	children: DateLink[]
 
-  constructor(private route: ActivatedRoute) {}
+	constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.update(this.date)
-    this.route.params.subscribe(params => this.updateDate(params))
-  }
+	ngOnInit() {
+		this.update(this.date)
+		this.route.params.subscribe(params => this.updateDate(params))
+	}
 
-  private updateDate(params: any) {
-    let date = this.switcher.switch(new DateExtractor(), params)
-    this.update(date)
-  }
+	private updateDate(params: any) {
+		let date = this.switcher.switch(new DateExtractor(), params)
+		this.update(date)
+	}
 
-  private update(date: Date) {
-    this.dateLink = DateLink.create(this.switcher, date)
+	private update(date: Date) {
+		this.dateLink = DateLink.create(this.switcher, date)
 
-    let nextDate = this.switcher.switch(new NextDate(), date)
-    this.nextDateLink = DateLink.create(this.switcher, nextDate)
+		let nextDate = this.switcher.switch(new NextDate(), date)
+		this.nextDateLink = DateLink.create(this.switcher, nextDate)
 
-    let prevDate = this.switcher.switch(new PreviousDate(), date)
-    this.prevDateLink = DateLink.create(this.switcher, prevDate)
+		let prevDate = this.switcher.switch(new PreviousDate(), date)
+		this.prevDateLink = DateLink.create(this.switcher, prevDate)
 
-    this.parent = this.switcher.switch(new ParentLink(), date)
-    this.children = this.switcher.switch(new ChildrenLinks(), date)
-    
-    $(".dropdown-button").dropdown({
-      constrainWidth: false,
-      hover: true
-    })
-  }
+		this.parent = this.switcher.switch(new ParentLink(), date)
+		this.children = this.switcher.switch(new ChildrenLinks(), date)
+		
+		$(".dropdown-button").dropdown({
+			constrainWidth: false,
+			hover: true
+		})
+	}
 }
 
 class DateLink {
-  label: string
-  urlSuffix: string
+	label: string
+	urlSuffix: string
 
-  static create(switcher: BudgetPeriodSwitcher | BudgetPeriod, date: Date) {
-    if (!(switcher instanceof BudgetPeriodSwitcher)) {
-      switcher = new BudgetPeriodSwitcher(switcher)
-    }
-    return switcher.switch(new DateLinkFactory(), date)
-  }
+	static create(switcher: BudgetPeriodSwitcher | BudgetPeriod, date: Date) {
+		if (!(switcher instanceof BudgetPeriodSwitcher)) {
+			switcher = new BudgetPeriodSwitcher(switcher)
+		}
+		return switcher.switch(new DateLinkFactory(), date)
+	}
 }
 
 class ChildrenLinks implements BudgetPeriodSwitch<Date, DateLink[]> {
  
-  caseMonthly(arg: Date): DateLink[] {
-    return null //There is no finer view than 'monthly'
-  }
+	caseMonthly(arg: Date): DateLink[] {
+		return null //There is no finer view than 'monthly'
+	}
 
-  caseYearly(arg: Date): DateLink[] {
-    let result = []
-    for (let month = 0; month < 12; month++) {
-      let date = new Date(arg.getFullYear(), month)
-      result.push(DateLink.create(BudgetPeriod.MONTHLY, date))
-    }
-    return result
-  }
+	caseYearly(arg: Date): DateLink[] {
+		let result = []
+		for (let month = 0; month < 12; month++) {
+			let date = new Date(arg.getFullYear(), month)
+			result.push(DateLink.create(BudgetPeriod.MONTHLY, date))
+		}
+		return result
+	}
 }
 
 class ParentLink implements BudgetPeriodSwitch<Date, DateLink> {
 
-  caseMonthly(arg: Date): DateLink {
-    let year = new Date(arg.getFullYear(), 0)
-    return DateLink.create(BudgetPeriod.YEARLY, year)
-  }
+	caseMonthly(arg: Date): DateLink {
+		let year = new Date(arg.getFullYear(), 0)
+		return DateLink.create(BudgetPeriod.YEARLY, year)
+	}
 
-  caseYearly(arg: Date): DateLink {
-    return null //There is no coarser view than 'yearly'
-  }
+	caseYearly(arg: Date): DateLink {
+		return null //There is no coarser view than 'yearly'
+	}
 }
 
 class DateLinkFactory implements BudgetPeriodSwitch<Date, DateLink> {
 
-  caseMonthly(arg: Date): DateLink {
-    return {
-      label: `${arg.getMonthName()} ${arg.getFullYear()}`,
-      urlSuffix: `year/${arg.getFullYear()}/month/${arg.getMonth()}`
-    }
-  }
+	caseMonthly(arg: Date): DateLink {
+		return {
+			label: `${arg.getMonthName()} ${arg.getFullYear()}`,
+			urlSuffix: `year/${arg.getFullYear()}/month/${arg.getMonth()}`
+		}
+	}
 
-  caseYearly(arg: Date): DateLink {
-    return {
-      label: `${arg.getFullYear()}`,
-      urlSuffix: `year/${arg.getFullYear()}`
-    }
-  }
+	caseYearly(arg: Date): DateLink {
+		return {
+			label: `${arg.getFullYear()}`,
+			urlSuffix: `year/${arg.getFullYear()}`
+		}
+	}
 }
 
 class NextDate implements BudgetPeriodSwitch<Date, Date> {
 
-  caseMonthly(arg: Date): Date {
-    return new Date(arg.getFullYear(), arg.getMonth() + 1)
-  }
+	caseMonthly(arg: Date): Date {
+		return new Date(arg.getFullYear(), arg.getMonth() + 1)
+	}
 
-  caseYearly(arg: Date): Date {
-    return new Date(arg.getFullYear() + 1, 0)
-  }
+	caseYearly(arg: Date): Date {
+		return new Date(arg.getFullYear() + 1, 0)
+	}
 }
 
 class PreviousDate implements BudgetPeriodSwitch<Date, Date> {
 
-  caseMonthly(arg: Date): Date {
-    return new Date(arg.getFullYear(), arg.getMonth() - 1)
-  }
+	caseMonthly(arg: Date): Date {
+		return new Date(arg.getFullYear(), arg.getMonth() - 1)
+	}
 
-  caseYearly(arg: Date): Date {
-    return new Date(arg.getFullYear() - 1, 0)
-  }
+	caseYearly(arg: Date): Date {
+		return new Date(arg.getFullYear() - 1, 0)
+	}
 }
