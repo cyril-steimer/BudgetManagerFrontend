e63a5a544e5265edf9d36051581c61cec45e374d
Cyril Steimer <cyril.steimer@gmail.com>
2018-12-28 12:16:44 +0100

    Add an import feature.
    Still to be polished, error handling to be added.

--- src/app/import-export/import-export.component.ts
+++ src/app/import-export/import-export.component.ts
@@ -6,6 +6,12 @@ import * as saveAs from 'file-saver';
 
 const VERSION = '1.0';
 
+interface ImportExport {
+	version: string
+	expenses: Expense[]
+	budgets: Budget[]
+}
+
 @Component({
 	selector: 'app-import-export',
 	templateUrl: './import-export.component.html',
@@ -13,16 +19,55 @@ const VERSION = '1.0';
 })
 export class ImportExportComponent implements OnInit {
 
-	constructor(
+	constructor (
 		private expenseService: ExpenseService, 
 		private budgetService: BudgetService) { }
 
-	ngOnInit() {
+	ngOnInit () {
 
 	}
 
-	export() {
+	export () {
 		this.fetchExpenses();
+	}
+
+	import () {
+		let file = this.getFileToImport();
+		if (file == null) {
+			alert('No file selected');
+			return;
+		}
+		let reader = new FileReader();
+		reader.onload = () => this.checkContent(reader.result as string);
+		reader.readAsText(file);
+	}
+
+	private checkContent (text: string) {
+		let content = JSON.parse(text) as ImportExport;
+		if (content.version != null && content.expenses != null && content.budgets != null) {
+			this.importExpenses(content, 0);
+		}
+	}
+
+	private importExpenses (content: ImportExport, index: number) {
+		if (index >= content.expenses.length) {
+			this.importBudgets(content, 0);
+		}
+		this.expenseService.addExpense(content.expenses[index])
+			.subscribe(() => this.importExpenses(content, index + 1));
+	}
+
+	private importBudgets (content: ImportExport, index: number) {
+		if (index >= content.budgets.length) {
+			alert('Import is done!');
+		}
+		this.budgetService.addBudget(content.budgets[index])
+			.subscribe(() => this.importBudgets(content, index + 1));
+	}
+
+	private getFileToImport () {
+		let input = document.getElementById('import-file') as HTMLInputElement;
+		return input.files.length == 0 ? null : input.files[0];
 	}
 
 	private fetchExpenses () {
@@ -36,7 +81,7 @@ export class ImportExportComponent implements OnInit {
 	}
 
 	private downloadFile (expenses: Expense[], budgets: Budget[]) {
-		let content = {
+		let content: ImportExport = {
 			version: VERSION,
 			expenses: expenses,
 			budgets: budgets
