ea8c5a4987d453bb65b81d120b26796ef954879e
Cyril Steimer <cyril.steimer@ergon.ch>
2023-03-29 17:14:03 +0200

    Delete all the code and provide initial setup for react

--- src/app/date.extensions.impl.ts
+++
@@ -1,6 +0,0 @@
-const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-    'August', 'September', 'October', 'November', 'December'];
-
-Date.prototype.getMonthName = function (this: Date) {
-    return monthNames[this.getMonth()];
-};
