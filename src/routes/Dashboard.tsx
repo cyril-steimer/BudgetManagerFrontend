953cbb55ec63d1e4177d3ede6136845553243721
Cyril Steimer <cyril.steimer@ergon.ch>
2023-04-17 20:22:51 +0200

    Add cards to the dashboard
    Most of the buttons on the dashboard are not yet functioning, but we can see how the dashboard might look in the future and make it easier to see which features must still be implemented.

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,18 +1,54 @@
 import {useNavigate} from 'react-router-dom';
 import dayjs from 'dayjs';
 import {useIsNavigating} from '../hooks/hooks';
-import {Button, ButtonGroup} from '@mui/material';
+import {Button, Card, CardActions, CardContent, CardHeader, cardHeaderClasses, Typography} from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import {styled} from '@mui/material/styles';
+
+const StyledCardHeader = styled(CardHeader)(({theme}) => ({
+    [`&.${cardHeaderClasses.root}`]: {
+        backgroundColor: theme.palette.action.hover
+    }
+}));
+
+class CardButton {
+    constructor(readonly text: string, readonly action: () => void) {
+    }
+}
+
+interface CardParameters {
+    title: string;
+    text: string;
+    buttons: CardButton[];
+}
+
+function DashboardCard({title, text, buttons}: CardParameters) {
+    const disableButtons = useIsNavigating();
+
+    return (
+        <Grid2 xs={6}>
+            <Card variant="outlined">
+                <StyledCardHeader title={<Typography variant="h6">{title}</Typography>}></StyledCardHeader>
+                <CardContent>
+                    <Typography>{text}</Typography>
+                </CardContent>
+                <CardActions>
+                    {buttons.map(b => <Button key={b.text} type="button" onClick={b.action} disabled={disableButtons}>{b.text}</Button>)}
+                </CardActions>
+            </Card>
+        </Grid2>
+    );
+}
 
 export default function Dashboard() {
     const navigate = useNavigate();
-    const disableButtons = useIsNavigating();
 
     function viewThisMonth() {
-        viewMonthlyExpenses(dayjs())
+        viewMonthlyExpenses(dayjs());
     }
 
-    function viewLastMonth() {
-        viewMonthlyExpenses(dayjs().subtract(1, 'month'))
+    function notYetImplemented() {
+        alert('Not yet implemented');
     }
 
     function viewMonthlyExpenses(date: dayjs.Dayjs) {
@@ -21,9 +57,45 @@ export default function Dashboard() {
     }
 
     return (
-        <ButtonGroup variant="outlined" disabled={disableButtons}>
-            <Button type="button" onClick={viewLastMonth}>Last Month</Button>
-            <Button type="button" onClick={viewThisMonth}>This Month</Button>
-        </ButtonGroup>
+        <Grid2 container spacing={2}>
+            <DashboardCard
+                title="Expenses"
+                text="View the list of all expenses during a certain time frame"
+                buttons={[
+                    new CardButton('This Month', viewThisMonth),
+                    new CardButton('This Year', notYetImplemented),
+                    new CardButton('All Time', notYetImplemented)
+                ]}
+            />
+            <DashboardCard
+                title="Budget"
+                text="Check the state of your budget during the current month or year"
+                buttons={[
+                    new CardButton('This Month', notYetImplemented),
+                    new CardButton('This Year', notYetImplemented)
+                ]}
+            />
+            <DashboardCard
+                title="Import/Export"
+                text="Import or export all expense/budget data. The data can then be used in another instance of the budget manager"
+                buttons={[
+                    new CardButton('Import/Export', notYetImplemented)
+                ]}
+            />
+            <DashboardCard
+                title="Templates"
+                text="View the list of templates"
+                buttons={[
+                    new CardButton('All Templates', notYetImplemented)
+                ]}
+            />
+            <DashboardCard
+                title="Scheduled Expenses"
+                text="View the list of scheduled expenses"
+                buttons={[
+                    new CardButton('All Scheduled Expenses', notYetImplemented)
+                ]}
+            />
+        </Grid2>
     );
 }
