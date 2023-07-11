f1825be4abc76429faf602490cfba49992279709
Cyril Steimer <cyril.steimer@ergon.ch>
2023-07-11 09:42:07 +0200

    Improved tag editor

--- src/components/Expense-Editor.tsx
+++ src/components/Expense-Editor.tsx
@@ -1,6 +1,6 @@
 import {Expense} from "../model/expense";
 import {useState} from "react";
-import {Box, Chip, IconButton, InputAdornment, Stack, TextField} from "@mui/material";
+import {Box, Button, Chip, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Stack, TextField, Typography} from "@mui/material";
 import {NamedObject} from "../model/common";
 import {DateStructPicker} from "./Editor";
 import AddIcon from "@mui/icons-material/Add";
@@ -14,6 +14,9 @@ function namedObject(value: string): NamedObject {
 export function ExpenseEditor({initialExpense}: {initialExpense: Expense}) {
     const [expense, setExpense] = useState(initialExpense);
     const [currentTag, setCurrentTag] = useState('');
+    const [openAddTag, setOpenAddTag] = useState(false);
+
+    const canAddTag = currentTag !== '' && expense.tags.find(tag => tag.name === currentTag) === undefined;
 
     function set<K extends keyof Expense>(key: K, value: Expense[K]) {
         const copy = {...expense};
@@ -22,7 +25,7 @@ export function ExpenseEditor({initialExpense}: {initialExpense: Expense}) {
     }
 
     function addTag() {
-        if (currentTag === '') {
+        if (!canAddTag) {
             return;
         }
         const newTags = [...expense.tags];
@@ -34,6 +37,10 @@ export function ExpenseEditor({initialExpense}: {initialExpense: Expense}) {
     function deleteTag(tag: NamedObject) {
         const newTags = [...expense.tags].filter(t => t.name !== tag.name);
         set('tags', newTags);
+    }
+
+    function closeAddTagDialog() {
+        setOpenAddTag(false);
     }
 
     return (
@@ -71,28 +78,43 @@ export function ExpenseEditor({initialExpense}: {initialExpense: Expense}) {
                 onChange={event => set('author', namedObject(event.target.value))}
                 margin='normal'
             />
-            <TextField
-                fullWidth
-                label='Tags'
-                value={currentTag}
-                onChange={event => setCurrentTag(event.target.value)}
-                InputProps={{
-                    endAdornment: (
-                        <InputAdornment position='end'>
-                            <IconButton onClick={addTag}>
-                                <AddIcon/>
-                            </IconButton>
-                        </InputAdornment>
-                    )
-                }}
-                margin='normal'
+            <Typography 
+                variant='body1'
+                sx={{marginTop: '10px', marginBottom: '10px'}}
             >
-            </TextField>
+                Tags
+            </Typography>
             <Stack direction='row' spacing={1}>
                 {expense.tags.map(tag => (
-                    <Chip label={tag.name} onDelete={() => deleteTag(tag)} variant='outlined'/>
+                    <Chip key={tag.name} label={tag.name} onDelete={() => deleteTag(tag)} variant='outlined'/>
                 ))}
+                <IconButton onClick={() => setOpenAddTag(true)}>
+                    <AddIcon/>
+                </IconButton>
             </Stack>
+            <Dialog open={openAddTag} onClose={closeAddTagDialog}>
+                <DialogTitle>Add Tag</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        fullWidth
+                        variant='standard'
+                        label='Tag'
+                        value={currentTag}
+                        onChange={event => setCurrentTag(event.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        disabled={!canAddTag}
+                        onClick={() => {
+                            addTag();
+                            closeAddTagDialog();
+                        }}
+                    >
+                        Add Tag
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 }
